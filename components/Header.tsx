26ab802f9be4ac18de5cb7326972d9100a345e6f
fobic <sabychakraborty08@gmail.com>
2024-07-28 17:22:21 +0530

    feat(Homepage) : added homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { Link } from 'lucide-react'
 import React, { Children } from 'react'
+import { cn } from '@/lib/utils'
 
-const Header = ({ children }: HeaderProps) => {
+const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className='header'> 
+    <div className={cn("header", className)}> 
       <Link href='/' className='md:flex-1'>
         <Image
             src='/assets/icons/logo.svg'
