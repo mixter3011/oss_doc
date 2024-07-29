26bb3f923a15006cacc88b17a7e61fd96af8ca9c
fobic <sabychakraborty08@gmail.com>
2024-07-29 10:45:34 +0530

    func(edit) : added title edit.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,24 @@
-import Image from 'next/image'
-import { Link } from 'lucide-react'
-import React, { Children } from 'react'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className={cn("header", className)}> 
-      <Link href='/' className='md:flex-1'>
-        <Image
-            src='/assets/icons/logo.svg'
-            alt='logo with name'
-            width={120}
-            height={32}
-            className='hidden md:block'
+    <div className={cn("header", className)}>
+      <Link href='/' className="md:flex-1">
+        <Image 
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
         />
-        <Image
-            src='/assets/icons/logo-icon.svg'
-            alt='logo'
-            width={32}
-            height={32}
-            className='mr-2 md:hidden'
+        <Image 
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
         />
       </Link>
       {children}
