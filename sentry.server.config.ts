7850fb53d2fa11405dc4a5ef80fa8a5f6e913b6c
fobic <sabychakraborty08@gmail.com>
2024-07-30 18:00:30 +0530

    func(sentry) : added sentry.

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://211c1aca274f6a68f923bf8b518c346f@o4507465486303232.ingest.de.sentry.io/4507690696114256",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
